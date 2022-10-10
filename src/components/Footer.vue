e0646239ec92871b783e3c2a32235ec12be66998
ZhengKe <zhengke996@163.com>
2022-10-10 15:06:13 +0800

    fix

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,7 @@
 <template>
   <div class="mt-10 mb-6 prose m-auto opacity-50 flex">
     <span class="text-sm">
-      <!-- <a
-        target="_blank"
-        href="https://beian.miit.gov.cn"
-        style="color: inherit"
-      >
-        浙ICP备2021022773号-2 &nbsp;&nbsp;
-      </a> -->
+      <a target="_blank" href="https://beian.miit.gov.cn" style="color: inherit"> 浙ICP备2021022773号-2 &nbsp;&nbsp; </a>
       2022-PRESENT © ZhengKe
     </span>
     <div class="flex-auto" />
