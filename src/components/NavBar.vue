69ceb6e6ea89057283e149d9a3bc64dcfafc45a6
Zhengke <zhengke996@163.com>
2023-05-17 14:57:58 +0800

    fix

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -31,6 +31,10 @@ import { isDark } from '~/logics'
           <span class="lt-md:hidden">ML</span>
           <ri-lightbulb-line class="md:hidden" />
         </router-link>
+        <router-link to="/algorithms" title="Algorithms">
+          <span class="lt-md:hidden">Algorithms</span>
+          <ri-lightbulb-line class="md:hidden" />
+        </router-link>
 
         <!-- <router-link to="/data-structure" title="DataStructure">
           <span class="lt-md:hidden">DataStructure</span>
